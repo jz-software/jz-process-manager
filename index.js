3f4cd5ab86fce85166b0acaa6c19c2512c65de3d
JMax45 <maxmailru.45@gmail.com>
2021-02-03 10:44:19 +0100

    feat: move to websockets

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-const axios = require('axios');
+const WebSocketClient = require('websocket').client;
 const { getCpuUsage, memoryUsage } = require('./helpers/os');
 
 class ProcessManager{
     constructor(server_url, application_id, auth_key, refreshRate){
-        const interval = setInterval(async function() {
-            const cpuUsage = await getCpuUsage();
-            axios.post(`${server_url}/application/edit/`, null, { params: {
-                application_id,
-                auth_key,
-                status: true,
-                cpu_usage: cpuUsage.toFixed(2),
-                ram_usage: memoryUsage().toFixed(2),
-            }})
-        }, refreshRate);
+        this.client = new WebSocketClient();
+        this.client.on('connectFailed', function(error) {
+            console.log('Connect Error: ' + error.toString());
+        });
+        this.client.on('connect', function(connection) {
+            console.log('WebSocket Client Connected');
+            connection.on('error', function(error) {
+                console.log("Connection Error: " + error.toString());
+            });
+            connection.on('close', function() {
+                console.log('echo-protocol Connection Closed');
+            });
+            connection.on('message', function(message) {
+                if (message.type === 'utf8') {
+                    const response = JSON.parse(message.utf8Data);
+                    if(response.status != 200){
+                        console.log("Received: '" + message.utf8Data + "'");
+                    }
+                }
+            });
+            
+            async function sendInfo() {
+                if (connection.connected) {
+                    const cpuUsage = await getCpuUsage();
+                    const info = {
+                        application_id,
+                        auth_key,
+                        status: true,
+                        cpu_usage: cpuUsage.toFixed(2),
+                        ram_usage: memoryUsage().toFixed(2),
+                    }
+                    connection.sendUTF(JSON.stringify(info));
+                    setTimeout(sendInfo, refreshRate);
+                }
+            }
+            sendInfo();
+        });
+        this.client.connect(server_url, 'echo-protocol');
     }
 }
 
